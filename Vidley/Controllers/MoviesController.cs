afbe223a305132864ed330057d0fbdcdcccb75ba
stuhoffman <stu@hoffmanswebsite.com>
2016-07-10 13:58:34 -0700

    Changes to match excercise challenge 2. Was missing the call in the Customers controller.

--- Vidley/Controllers/MoviesController.cs
+++ Vidley/Controllers/MoviesController.cs
@@ -11,6 +11,22 @@ namespace Vidley.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek" },
+                new Movie {Id = 2, Name = "Wall-o" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
